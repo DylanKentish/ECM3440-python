069bd5839cb154d76a74d9a4328237ff31b15071
Michael Saunby <mike@saunby.net>
2019-03-30 09:14:15 +0000

    .

--- static_typing/language_features/f_strings.py
+++ static_typing/language_features/f_strings.py
@@ -8,10 +8,11 @@ def fn():
 
     print(f"1. Today is {today}, and tomorrow is {tomorrow}.")
 
-    # The same can be achieved with the rather more verbose form
+    # The same can be achieved with the rather more verbose form.
+    # (Note that the ** operator unpacks a dictionary)
     print("2. Today is {today}, and tomorrow is {tomorrow}.".format(**locals(),**globals()))
     # Though I don't think anyone ever used that. More usual is
-    print("3. Today is {}, and tomorrow is {}.".format(today,tomorrow))
+    print("3. Today is {}, and tomorrow is {}.".format(today, tomorrow))
     # Or the traditional style
     print("4. Today is %s, and tomorrow is %s." % (today, tomorrow))
 
