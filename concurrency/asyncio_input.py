306999a07f4c850b448406c0d5df599fb893f159
Michael Saunby <mike@saunby.net>
2019-04-04 16:35:45 +0100

    .

--- concurrency/asyncio_input.py
+++ concurrency/asyncio_input.py
@@ -2,7 +2,6 @@ import asyncio
 import sys
 import threading
 
-
 class Screen:
     def __init__(self,width:int, height:int):
         self.width = width
@@ -12,7 +11,14 @@ class Screen:
     def clear(self) -> None:
         # Use list of lists of one character strings
         # to form a mutable array of strings
-        self.data = [[' '] * self.width] * self.height
+        self.data = []
+        for y in range(self.height):
+            self.data.append([' '] * self.width)
+
+    def draw(self,x:int, y:int, c:str) -> None:
+        row = self.data[y]
+        row[x] = c[0]
+        return
 
     def __repr__(self) -> str:
         out = ""
@@ -27,7 +33,9 @@ async def print_screen():
     global in_count, scr
     out_count = 0
     while True:
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.05)
+        scr.clear()
+        scr.draw(in_count%20,in_count%20,"*")
         print(scr)
         print(f"{in_count:04} {out_count:04}")
         out_count += 1
@@ -35,7 +43,7 @@ async def print_screen():
 async def update_game():
     global in_count
     while True:
-        await asyncio.sleep(0.01)
+        await asyncio.sleep(0.2)
         in_count += 1
 
             
